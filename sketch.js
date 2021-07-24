6bd9643421dcaf4d703818e7ec51c3cbdad9df71
PrathamKathuria <87126893+PrathamKathuria@users.noreply.github.com>
2021-07-24 15:55:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function setup() {
+  createCanvas(400,400);
+
+
+}
+
+function draw() 
+{
+  mousePressed();
+}
+
+function mousePressed() {
+  background(rgb(RandomNumber(2,25),RandomNumber(3,232),RandomNumber(55,240)));
+}
